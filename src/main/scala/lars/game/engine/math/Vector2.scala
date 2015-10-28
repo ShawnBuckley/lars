1bb294c94f48d5e2ee063d0df12a5b8a0b4f15a4
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-10-28 18:20:32 -0400

    Change Vector2 to use Long

--- src/main/scala/lars/game/engine/math/Vector2.scala
+++ src/main/scala/lars/game/engine/math/Vector2.scala
@@ -1,6 +1,6 @@
 package lars.game.engine.math
 
-case class Vector2(x: Int, y: Int) {
+case class Vector2(x: Long, y: Long) {
   def +(vec: Vector2) : Vector2 = new Vector2(x + vec.x, y + vec.y)
   def -(vec: Vector2) : Vector2 = new Vector2(x - vec.x, y + vec.y)
   def ==(vec: Vector2) : Boolean = (x == vec.x && y == vec.y)
