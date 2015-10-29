d28c0ac0b939b68b25f6a1f168e16ed5a9eb2554
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-10-29 18:12:47 -0400

    Add basic MassiveBody implementation

---
+++ src/main/scala/lars/game/engine/celestial/body/MassiveBody.scala
@@ -0,0 +1,21 @@
+package lars.game.engine.celestial.body
+
+import lars.game.engine.celestial.Massive
+import lars.game.engine.math.Vector2
+
+class MassiveBody(_mass: Long, _location: Vector2, _drift: Vector2) extends Massive {
+  var loc = _location
+  var vel = _drift
+
+  override def mass: Long = _mass
+
+  override def location_=(_loc: Vector2): Unit = loc = _loc
+
+  override def location: Vector2 = location
+
+  override def observe(): Unit = {}
+
+  override def drift_=(vec: Vector2): Unit = vel = vec
+
+  override def drift: Vector2 = vel
+}
