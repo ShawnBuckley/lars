b39e550b7afddf4faab4e7254febf4f717abab38
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-10-29 23:52:23 -0400

    Add Vector2 identity

--- src/main/scala/lars/game/engine/math/Vector2.scala
+++ src/main/scala/lars/game/engine/math/Vector2.scala
@@ -5,3 +5,7 @@ case class Vector2(x: Long, y: Long) {
   def -(vec: Vector2) : Vector2 = new Vector2(x - vec.x, y + vec.y)
   def ==(vec: Vector2) : Boolean = (x == vec.x && y == vec.y)
 }
+
+object Vector2 {
+  val identity = new Vector2(0,0)
+}
