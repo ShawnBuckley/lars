b188fdd7e13cdfa90bc9959ddf3031c97a2214fa
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-03 07:06:34 -0500

    Vary massives in test

--- src/test/scala/PhysicsTest.scala
+++ src/test/scala/PhysicsTest.scala
@@ -5,20 +5,18 @@ import org.testng.annotations.Test
 object PhysicsTest {
   @Test
   def barycenter(): Boolean = {
-    val m1 = new MassiveBody(1000, new Vector2(1,0))
+    val m1 = new MassiveBody(10000, new Vector2(10,0))
     val m2 = new MassiveBody(1000, new Vector2(0,1))
-    val barycenter = Physics.barycenter(m1, m2)
-    barycenter == Vector2.identity
+    Physics.barycenter(m1, m2) == Vector2.identity
   }
 
   @Test
   def barycenterSeq(): Boolean = {
-    val m1 = new MassiveBody(1000, new Vector2(1,0))
-    val m2 = new MassiveBody(1000, new Vector2(0,1))
-    val m3 = new MassiveBody(1000, new Vector2(-1,0))
-    val m4 = new MassiveBody(1000, new Vector2(0,-1))
+    val m1 = new MassiveBody(5000, new Vector2(5,0))
+    val m2 = new MassiveBody(4000, new Vector2(0,4))
+    val m3 = new MassiveBody(8000, new Vector2(-8,0))
+    val m4 = new MassiveBody(3000, new Vector2(0,-3))
     val massives = Array(m1, m2, m3, m4)
-    val barycenter = Physics.barycenter(massives)
-    barycenter == Vector2.identity
+    Physics.barycenter(massives) == Vector2.identity
   }
 }
