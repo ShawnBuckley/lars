5287f56cb17cb3a245b3cba7de554a8ccd871bb1
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-03 22:45:29 -0500

    Fix vector subtraction

--- src/main/scala/lars/game/engine/math/Vector2.scala
+++ src/main/scala/lars/game/engine/math/Vector2.scala
@@ -2,7 +2,7 @@ package lars.game.engine.math
 
 case class Vector2(x: Long, y: Long) {
   def +(vec: Vector2): Vector2 = new Vector2(x + vec.x, y + vec.y)
-  def -(vec: Vector2): Vector2 = new Vector2(x - vec.x, y + vec.y)
+  def -(vec: Vector2): Vector2 = new Vector2(x - vec.x, y - vec.y)
   def *(factor: Int): Vector2 = new Vector2(x * factor, y * factor)
   def /(factor: Int): Vector2 = new Vector2(x / factor, y / factor)
   def ==(vec: Vector2) : Boolean = (x == vec.x && y == vec.y)
