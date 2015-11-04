2f84b110a8199a31bef2830645d4bfef56faa01a
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-03 21:37:21 -0500

    Simplify barycenter testing

--- src/test/scala/PhysicsTest.scala
+++ src/test/scala/PhysicsTest.scala
@@ -3,19 +3,23 @@ import lars.game.engine.math.{Physics, Vector2}
 import org.testng.annotations.Test
 
 object PhysicsTest {
+  def createBody(factor: Int, loc: Vector2): MassiveBody = {
+    new MassiveBody(1000 * factor, loc * factor)
+  }
+
   @Test
   def barycenter(): Boolean = {
-    val m1 = new MassiveBody(10000, new Vector2(10,0))
-    val m2 = new MassiveBody(1000, new Vector2(0,1))
+    val m1 = createBody(10, new Vector2(1,0))
+    val m2 = createBody(1, new Vector2(0,1))
     Physics.barycenter(m1, m2) == Vector2.identity
   }
 
   @Test
   def barycenterSeq(): Boolean = {
-    val m1 = new MassiveBody(5000, new Vector2(5,0))
-    val m2 = new MassiveBody(4000, new Vector2(0,4))
-    val m3 = new MassiveBody(8000, new Vector2(-8,0))
-    val m4 = new MassiveBody(3000, new Vector2(0,-3))
+    val m1 = createBody(5, new Vector2(1,0))
+    val m2 = createBody(4, new Vector2(0,4))
+    val m3 = createBody(8, new Vector2(-1,0))
+    val m4 = createBody(3, new Vector2(0,-1))
     val massives = Array(m1, m2, m3, m4)
     Physics.barycenter(massives) == Vector2.identity
   }
