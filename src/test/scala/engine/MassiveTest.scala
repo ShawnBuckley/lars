e562d310437a9147b94b82646795a2963b5f98cc
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-04 23:15:22 -0500

    Rename MassiveBodyTest to MassiveTest, add companion object tests

--- src/test/scala/engine/MassiveTest.scala
+++ src/test/scala/engine/MassiveTest.scala
@@ -1,9 +1,10 @@
+import lars.game.engine.celestial.Massive
 import lars.game.engine.celestial.body.MassiveBody
 import lars.game.engine.math.Vector2
 import org.testng.annotations.Test
 import org.testng.Assert._
 
-class MassiveBodyTest {
+class MassiveTest {
   val smaller = new MassiveBody(1000, Vector2.addIdent)
   val larger = new MassiveBody(10000, Vector2.addIdent)
 
@@ -23,5 +24,24 @@ class MassiveBodyTest {
   def <=() = {
     assertEquals(smaller <= larger, true)
     assertEquals(smaller <= smaller, true)
+  }
+
+  @Test
+  def min() = {
+    assertEquals(Massive.min(smaller, larger), smaller)
+    assertEquals(Massive.min(larger, smaller), smaller)
+  }
+
+  @Test
+  def max() = {
+    assertEquals(Massive.max(larger, smaller), larger)
+    assertEquals(Massive.max(smaller, larger), larger)
+  }
+
+  @Test
+  def sort() = {
+    val result = (larger, smaller)
+    assertEquals(Massive.sort(larger, smaller), result)
+    assertEquals(Massive.sort(smaller, larger), result)
   }
 }
