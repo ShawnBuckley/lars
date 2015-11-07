65706531289feff50903b9a274c0dd7f2813698b
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-07 16:03:22 -0500

    Fix microbody

--- src/main/scala/lars/game/engine/celestial/body/MicroBody.scala
+++ src/main/scala/lars/game/engine/celestial/body/MicroBody.scala
@@ -1,5 +1,12 @@
 package lars.game.engine.celestial.body
 
-class MicroBody extends CelestialBody() {
+import lars.game.engine.celestial.{Sizeable, Parent}
+import lars.game.engine.math.Vector2
 
+class MicroBody(mass: Long, loc: Vector2, size: Long) extends CelestialBody(mass, loc, size) {
+  override def observe(): Unit = ???
+
+  override def collide(other: Sizeable, velocity: Vector2): Unit = ???
+
+  override var par: Parent = _
 }
