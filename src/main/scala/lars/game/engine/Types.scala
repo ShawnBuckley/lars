abda8d4d076830a58321dc4de21147871cb61b2c
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-07 20:02:29 -0500

    Add type conversions

--- src/main/scala/lars/game/engine/Types.scala
+++ src/main/scala/lars/game/engine/Types.scala
@@ -5,4 +5,16 @@ object Types {
   type DistanceType = Long
   type VolumeType = Double
   type DensityType = Double
+
+  def toMass[T](value: T): MassType =
+    value.asInstanceOf[Number].longValue
+
+  def toDistance[T](value: T): DistanceType =
+    value.asInstanceOf[Number].longValue
+
+  def toVolume[T](value: T): VolumeType =
+    value.asInstanceOf[Number].doubleValue
+
+  def toDensity[T](value: T): DensityType =
+    value.asInstanceOf[Number].doubleValue
 }
