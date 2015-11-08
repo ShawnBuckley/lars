0a49d5ad1c82d09f332128d506ddedaff74c2db7
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-07 22:18:53 -0500

    Add doc link to CelestialBody javadoc

--- src/main/scala/lars/game/engine/celestial/body/base/CelestialBody.scala
+++ src/main/scala/lars/game/engine/celestial/body/base/CelestialBody.scala
@@ -6,7 +6,9 @@ import lars.game.engine.physics.units.{Density, Length, Mass}
 
 /**
   * Celestial bodies are an abstract base class used for the creation of other objects.  These include stellar, gaseous,
-  * terrestrial, and micro bodies.  Celestial bodies are standard (massive and sizeable) objects
+  * terrestrial, and micro bodies.  Celestial bodies are standard (massive and sizeable) objects.
+  *
+  * See: https://docs.google.com/document/d/1uG7m4KMdTkZvW4E3ZjkSSvqP9JJVgQFqXzz1hyGJf7s
   *
   * @param _mass
   * @param _loc
