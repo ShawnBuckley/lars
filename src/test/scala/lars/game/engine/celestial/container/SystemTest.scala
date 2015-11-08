3e9c05a795f8e6425f14be6a1ab15eab1a7f42d9
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-08 16:43:51 -0500

    Add System mass test

---
+++ src/test/scala/lars/game/engine/celestial/container/SystemTest.scala
@@ -0,0 +1,26 @@
+package lars.game.engine.celestial.container
+
+import lars.Game
+import lars.game.engine.Constants
+import lars.game.engine.celestial.body.base.CelestialBody
+import lars.game.engine.math.Vector2
+import lars.game.engine.physics.units.Mass
+import org.testng.annotations.Test
+import org.testng.Assert._
+
+class SystemTest {
+  @Test
+  def mass(): Unit = {
+    val system = new System(Vector2.addIdent, Game.galaxy)
+    val sun = new CelestialBody(Constants.Sol.sol.mass, new Vector2(0,0), Constants.Sol.sol.radius, system)
+    val earth = new CelestialBody(Constants.Sol.earth.mass, new Vector2(Constants.Sol.earth.orbit.radius.km,0), Constants.Sol.earth.radius, system)
+    system.add(sun)
+    assertEquals(system.mass, sun.mass)
+    system.add(earth)
+    assertEquals(system.mass, sun.mass + earth.mass)
+    system.del(earth)
+    assertEquals(system.mass, sun.mass)
+    system.del(sun)
+    assertEquals(system.mass.kg, Mass.zero)
+  }
+}
