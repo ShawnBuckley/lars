4bc1875611dd7ec9e6350d4e20c2240e88e9e684
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-10 22:37:48 -0500

    Add SphereTest

--- src/test/scala/lars/game/engine/math/SphereTest.scala
+++ src/test/scala/lars/game/engine/math/SphereTest.scala
@@ -1,10 +1,12 @@
 package lars.game.engine.math
 
+import lars.game.engine.physics.units.{Volume, Length}
 import org.testng.annotations.Test
+import org.testng.Assert._
 
 class SphereTest {
   @Test
   def volume() = {
-
+    assertEquals(Sphere.volume(new Length(10)), new Volume(math.Pi * 1e3))
   }
 }
