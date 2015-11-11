9c7f1794ec994e442fddb4d0d670c058fc454b1f
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-10 22:37:38 -0500

    Use more accurate sphere volume calculation

--- src/main/scala/lars/game/engine/math/Sphere.scala
+++ src/main/scala/lars/game/engine/math/Sphere.scala
@@ -4,5 +4,5 @@ import lars.game.engine.physics.units.{Length, Volume}
 
 object Sphere {
   def volume(r: Length): Volume =
-    new Volume(1.333333333*math.Pi*math.pow(r.km,3))
+    new Volume((4/3)*math.Pi*math.pow(r.km,3))
 }
