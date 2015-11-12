e560f73d2e37719a07de1740164890293191997d
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-11 22:27:51 -0500

    Add accel to velocity conversion

--- src/main/scala/lars/game/engine/physics/units/Acceleration.scala
+++ src/main/scala/lars/game/engine/physics/units/Acceleration.scala
@@ -2,4 +2,7 @@ package lars.game.engine.physics.units
 
 import lars.game.engine.math.Vector2
 
-case class Acceleration(ms2: Vector2)
+case class Acceleration(ms2: Vector2) {
+  def /(that: Time): Velocity =
+    new Velocity(ms2 * that.s)
+}
