40e14b718eedef83838f147ea1357525ef66952f
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-11 23:46:12 -0500

    Add time week calculation, fix other values

--- src/main/scala/lars/game/engine/physics/units/Time.scala
+++ src/main/scala/lars/game/engine/physics/units/Time.scala
@@ -4,26 +4,51 @@ import lars.game.engine.physics.units.Time.TimeType
 
 case class Time(d: TimeType) {
   def us: TimeType =
-    d * 86400000000L
+    d * Time.d.us
 
   def ms: TimeType =
-    d * 86400000
+    d * Time.d.ms
 
   def s: TimeType =
-    d * 86400
+    d * Time.d.s
 
   def m: TimeType =
-    d * 1440
+    d * Time.d.m
 
   def h: TimeType =
-    d / 24
+    d * Time.d.h
+
+  def w: TimeType =
+    d * Time.d.w
 
   def y: TimeType =
-    d * 365
+    d * Time.d.y
+
+  def +(that: Time): Time =
+    new Time(d + that.d)
 }
 
 object Time {
   type TimeType = Double
-
   val zero: TimeType = 0
+
+  object in {
+    def s(sec: TimeType): Time =
+      new Time(sec / d.s)
+
+    def m(min: TimeType): Time =
+      new Time(min / d.m)
+  }
+
+  object d {
+    val us = 86400000000L
+    val ms = 86400000
+    val s = 86400
+    val m = 1440
+    val h = 24
+    val w = 1/7
+    val y = 1/365
+  }
+
+
 }
