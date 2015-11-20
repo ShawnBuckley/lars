c5cc269303bf7891889b6c027075555ceeed083c
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-20 18:27:43 -0500

    Add canvas click and drag movement

--- src/main/webapp/planets.js
+++ src/main/webapp/planets.js
@@ -7,6 +7,16 @@ console.log(pixelDistance)
 var canvas
 var context
 
+var mouseDown = false
+
+var viewport = {}
+viewport.x = 0
+viewport.y = 0
+
+var previousLocation = {}
+previousLocation.x = 0
+previousLocation.y = 0
+
 var planet_sprites = []
 planet_sprites[0] = {
     'color': 'yellow',
@@ -46,7 +56,29 @@ planet_sprites[8] = {
 }
 
 function loaded() {
-    $('#planets').css('background-color', 'rgba(0, 0, 0, 0.7)');
+    var jqPlanets = $('#planet')
+    $('#planets').css('background-color', 'rgba(0, 0, 0, 0.7)')
+    .mousedown(function(event) {
+        mouseDown = true
+        previousLocation.x = event.pageX
+        previousLocation.y = event.pageY
+    })
+    .mouseup(function() {
+        mouseDown = false
+    })
+    .blur(function() {
+        mouseDown = false
+    })
+    .mousemove(function(event) {
+        if(mouseDown == true) {
+            var diffX = (event.pageX - previousLocation.x)
+            var diffY = (event.pageY - previousLocation.y)
+            viewport.x += diffX
+            viewport.y += diffY
+            previousLocation.x = event.pageX
+            previousLocation.y = event.pageY
+        }
+    })
 
     canvas = document.getElementById("planets");
     context = canvas.getContext("2d");
@@ -59,8 +91,8 @@ function renderPlanets() {
         context.clearRect(0, 0, canvas.width, canvas.height);
         for(var i=0; i<data.length; i++) {
             context.beginPath();
-            var x = convert(data[i]["x"])
-            var y = convert(data[i]["y"])
+            var x = convert(data[i].x) + viewport.x
+            var y = convert(data[i].y) + viewport.y
             context.arc(x, y, planet_sprites[i].size, 0, 2 * Math.PI, false);
             context.fillStyle = planet_sprites[i].color;
             context.fill();
