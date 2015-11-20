dd7a0e59aaa23c4416d8470d09faf2c50ec13c07
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-19 22:37:31 -0500

    Add basic canvas display for planets

--- src/main/scala/lars/Main.scala
+++ src/main/scala/lars/Main.scala
@@ -14,53 +14,54 @@ object Main {
     val server = new EmbeddedWebapp
     server.start()
 
-//    def createPlanet(body: Body): TerrestrialBody = {
-//      new TerrestrialBody(
-//        body.mass,
-//        new Vector2(body.orbit.radius.km,0),
-//        new Velocity(new Vector2(0, body.orbit.speed.ms)),
-//        body.radius,
-//        system)
-//    }
-//
-//    val sun = system.add(new StellarBody(
-//      Constants.Sol.sol.mass,
-//      new Vector2(0.0,0.0),
-//      Constants.Sol.sol.radius,
-//      system))
-//    val mercury = system.add(createPlanet(Constants.Sol.mercury))
-//    val earth = system.add(createPlanet(Constants.Sol.earth))
-//    val mars = system.add(createPlanet(Constants.Sol.mars))
-//    val jupiter = system.add(createPlanet(Constants.Sol.jupiter))
+    def createPlanet(body: Body): TerrestrialBody = {
+      new TerrestrialBody(
+        body.mass,
+        new Vector2(body.orbit.radius.km,0),
+        new Velocity(new Vector2(0, body.orbit.speed.ms)),
+        body.radius,
+        system)
+    }
+
+    val sun = system.add(new StellarBody(
+      Constants.Sol.sol.mass,
+      new Vector2(0.0,0.0),
+      Constants.Sol.sol.radius,
+      system))
+    val mercury = system.add(createPlanet(Constants.Sol.mercury))
+    val venus = system.add(createPlanet(Constants.Sol.venus))
+    val earth = system.add(createPlanet(Constants.Sol.earth))
+    val mars = system.add(createPlanet(Constants.Sol.mars))
+    val jupiter = system.add(createPlanet(Constants.Sol.jupiter))
 //    val saturn = system.add(createPlanet(Constants.Sol.saturn))
 //    val neptune = system.add(createPlanet(Constants.Sol.neptune))
 //    val uranus = system.add(createPlanet(Constants.Sol.uranus))
-//
-//    // min, max orbital lengths
-//    var min = Double.MaxValue
-//    var max = 0.0
-//
-//    // state tracking
-//    var time = Time.second
-//    var count = 0
-//
-//    while(true) {
-//      // update state
-//      count += 1
-//      time += Time.second
-//
-//      system.observe()
-//
-//      // collect data
-//      val polar = Polar2.convert(uranus.location)
-//      val dist = new Length(polar.length).au // Constants.Sol.sol.radius.km
-//      min = math.min(min, dist)
-//      max = math.max(max, dist)
-//
-//      if(count % 1000000 == 0) {
-//        //
-//        println("Time: " + time.d + ", Angle: " + polar.angle + ", Dist: " + dist + ", min: " + min + ", max: " + max)
-//      }
-//    }
+
+    // min, max orbital lengths
+    var min = Double.MaxValue
+    var max = 0.0
+
+    // state tracking
+    var time = Time.second
+    var count = 0
+
+    while(true) {
+      // update state
+      count += 1
+      time += Time.second
+
+      system.observe()
+
+      // collect data
+      val polar = Polar2.convert(earth.location)
+      val dist = new Length(polar.length).au // Constants.Sol.sol.radius.km
+      min = math.min(min, dist)
+      max = math.max(max, dist)
+
+      if(count % 1000000 == 0) {
+        //
+        println("Time: " + time.d + ", Angle: " + polar.angle + ", Dist: " + dist + ", min: " + min + ", max: " + max)
+      }
+    }
   }
 }
---
+++ src/main/webapp/planets.js
@@ -0,0 +1,64 @@
+var canvasSize = 1000;
+// 25 au in km
+var dist = 3.73994677e9
+var pixelDistance = (dist * 2) / canvasSize
+console.log(pixelDistance)
+
+var canvas
+var context
+
+var planet_sprites = []
+planet_sprites[0] = {
+    'color': 'yellow',
+    'size': 5
+}
+planet_sprites[1] = {
+    'color': 'gray',
+    'size': 1
+}
+planet_sprites[2] = {
+    'color': 'orange',
+    'size': 2
+}
+planet_sprites[3] = {
+    'color': 'blue',
+    'size': 2
+}
+planet_sprites[4] = {
+    'color': 'red',
+    'size': 1
+}
+planet_sprites[5] = {
+    'color': 'brown',
+    'size': 4
+}
+
+
+function loaded() {
+    $('#planets').css('background-color', 'rgba(0, 0, 0, 0.7)');
+
+    canvas = document.getElementById("planets");
+    context = canvas.getContext("2d");
+
+    renderPlanets()
+}
+
+function renderPlanets() {
+    $.get("planets", function(data) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        for(var i=0; i<data.length; i++) {
+            context.beginPath();
+            var x = convert(data[i]["x"])
+            var y = convert(data[i]["y"])
+            context.arc(x, y, planet_sprites[i].size, 0, 2 * Math.PI, false);
+            context.fillStyle = planet_sprites[i].color;
+            context.fill();
+            context.lineWidth = 1;
+        }
+    });
+    setTimeout(renderPlanets, 10)
+}
+
+function convert(length) {
+    return (length + dist) / pixelDistance;
+}
