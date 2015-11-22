191acb34885d0aaa4d5563079818e65acb5619c6
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-22 14:42:11 -0500

    Change debug servlet to get and post for pause status

--- src/main/scala/lars/DebugServlet.scala
+++ src/main/scala/lars/DebugServlet.scala
@@ -2,8 +2,19 @@ package lars
 
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
 
+import org.json.JSONObject
+
 class DebugServlet extends HttpServlet {
-  override def service(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val status = new JSONObject
+    status.put("paused", Main.paused)
+
+    response.setContentType("application/json")
+    response.setCharacterEncoding("UTF-8")
+    response.getWriter.write(status.toString)
+  }
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     Main.paused = !Main.paused
   }
 }
