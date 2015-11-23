7ec3876ea1f2fd9fcd719a035958698ac4b2e606
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-22 23:10:48 -0500

    Switch planets table to use polar and AU

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -16,8 +16,10 @@ function createPlanetTable(id) {
             name.id = row.id + '_name'
             var x = row.insertCell(1)
             x.id = row.id + '_x'
+            x.className = 'planet-location'
             var y = row.insertCell(2)
             y.id = row.id + '_y'
+            y.className = 'planet-location'
             var button = document.createElement('button')
             button.className = 'planet-button'
             button.appendChild(document.createTextNode(planet.name))
@@ -33,8 +35,18 @@ function playpause() {
 }
 
 function updatePlanetsTable(planets) {
-    for(var i=0; i<planets.length; i++) {
-        $('#planets_table_' + i + '_x').text(planets[i].location.x)
-        $('#planets_table_' + i + '_y').text(planets[i].location.y)
+
+    // update sun location
+    var sun = planets[0].location
+    $('#planets_table_0_x').text(sun.x / 1.496e+8)
+    $('#planets_table_0_y').text(sun.y / 1.496e+8)
+
+    for(var i=1; i<planets.length; i++) {
+        var x = planets[i].location.x
+        var y = planets[i].location.y
+        var angle = Math.atan2(y, x)
+        var length = Math.sqrt(Math.pow(x - sun.x, 2) + Math.pow(y - sun.y, 2))
+        $('#planets_table_' + i + '_x').text(angle)
+        $('#planets_table_' + i + '_y').text(length / 1.496e+8)
     }
 }
