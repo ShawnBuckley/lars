f6f547f012f4c5b107465c96ab25030a2a74316a
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2015-11-23 20:32:17 -0500

    Switch to jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var jade = require('gulp-jade');
 
-gulp.task('default', function () {
+gulp.task('default', ['jade', 'typescript'], function() {});
+
+gulp.task('typescript', function () {
     return gulp.src('src/main/typescript/*.ts')
         .pipe(ts({
             noImplicitAny: true,
             out: 'compiled.js'
         }))
         .pipe(gulp.dest('src/main/webapp/'));
+});
+
+gulp.task('jade', function() {
+    gulp.src('./src/main/jade/*.jade')
+        .pipe(jade({
+            locals: {}
+        }))
+        .pipe(gulp.dest('./src/main/webapp/'))
 });
