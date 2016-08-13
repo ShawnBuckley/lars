370e58e313fabbfd14b4a4ffc8e45a0680da1cda
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2016-08-13 14:32:54 -0400

    Trying things

---
+++ core/src/main/scala/lars/frontend/Hello.scala
@@ -0,0 +1,11 @@
+//package lars.frontend
+//
+//import scala.scalajs.js.annotation.JSExport
+//
+//@JSExport
+//object Hello extends {
+//  @JSExport
+//  def hello(): Unit = {
+//    println("hello")
+//  }
+//}
