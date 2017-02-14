6423ff6cbb0a1345d69ded3c8fcc0ed3ccc5189b
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2017-02-14 12:12:06 -0500

    Add adding of massive bodies to a barycenter

--- core/src/main/scala/lars/core/physics/Barycenter.scala
+++ core/src/main/scala/lars/core/physics/Barycenter.scala
@@ -6,9 +6,14 @@ import lars.core.physics.units.{Mass, Velocity}
 
 /**
   * This is a computed barycenter.  It extends Massive and can be used as such.
-  * @param mass
-  * @param location
+  * @param mass total mass of the barycenter
+  * @param location calculated center of mass
   */
 case class Barycenter(override var mass: Mass, override var location: Vec2) extends Massive {
   override var velocity: Velocity = Velocity.zero
+
+  def add(massive: Massive): Unit = {
+    mass += massive.mass
+    location = ((location * mass.kg) + (massive.location * massive.mass.kg)) / mass.kg
+  }
 }
