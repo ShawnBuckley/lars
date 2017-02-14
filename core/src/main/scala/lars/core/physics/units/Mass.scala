d0d2c4f5e05761c78e5fd9cdfb2bfc3bb3be7e40
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2017-02-14 13:08:54 -0500

    Add docs to Mass and fix code style

--- core/src/main/scala/lars/core/physics/units/Mass.scala
+++ core/src/main/scala/lars/core/physics/units/Mass.scala
@@ -2,30 +2,34 @@ package lars.core.physics.units
 
 import lars.core.math.Vector1
 
+/**
+  * Mass unit type.  Ensures correct use in physics equations.
+  *
+  * @param kg mass in kilograms
+  */
 case class Mass(kg: Double) extends Vector1[Mass] {
-  override def +(that: Mass): Mass = new Mass(kg + that.kg)
-  override def -(that: Mass): Mass = new Mass(kg - that.kg)
-  override def *(scalar: Double): Mass = new Mass(kg * scalar)
-  override def /(scalar: Double): Mass = new Mass(kg / scalar)
+  override def +(that: Mass): Mass = Mass(kg + that.kg)
+  override def -(that: Mass): Mass = Mass(kg - that.kg)
+  override def *(scalar: Double): Mass = Mass(kg * scalar)
+  override def /(scalar: Double): Mass = Mass(kg / scalar)
   override def /(that: Mass): Double = kg / that.kg
 
-  override def unary_- : Mass = new Mass(-kg)
+  override def unary_- : Mass = Mass(-kg)
 
   override def compare(that: Mass): Int = kg.compare(that.kg)
 
-  override def midpoint(that: Mass): Mass = new Mass(Vector1.midpoint(kg, that.kg))
+  override def midpoint(that: Mass): Mass = Mass(Vector1.midpoint(kg, that.kg))
   override def distance(that: Mass): Double = Vector1.distance(kg, that.kg)
   override def magnitude: Double = kg
 
   // Conversion to other types
+  def /(that: Volume): Density = Density(kg / that.km3)
 
-  def /(that: Volume): Density = new Density(kg / that.km3)
-
-  def *(that: Velocity): Momentum = new Momentum(that.ms * kg)
+  def *(that: Velocity): Momentum = Momentum(that.ms * kg)
 }
 
 object Mass {
-  val zero: Mass = new Mass(0)
+  val zero: Mass = Mass(0)
 
   def min(m1: Mass, m2: Mass): Mass =
     if(m1 < m2) m1 else m2
@@ -35,8 +39,8 @@ object Mass {
 
   /**
     * Returns a tuple with the larger Mass first.
-    * @param m1
-    * @param m2
+    * @param m1 a mass
+    * @param m2 another mass
     * @return tuple, larger Mass first
     */
   def sort(m1: Mass, m2: Mass): (Mass, Mass) =
