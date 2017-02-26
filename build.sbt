fe65c31e42f2c5778e21768d462f55da487dbbe9
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2017-02-26 09:42:12 -0500

    Remove socket.io support

--- build.sbt
+++ build.sbt
@@ -22,17 +22,12 @@ lazy val project = crossProject.in(file(".")).
       "com.google.inject" % "guice" % "4.1.0",
       "com.google.inject.extensions" % "guice-multibindings" % "4.1.0",
 
-      "javax.websocket" % "javax.websocket-api" % "1.1",
-      "com.corundumstudio.socketio" % "netty-socketio" % "1.7.12",
-
       "org.testng" % "testng" % "6.10" % "test"
     )
   ).
   jsSettings(
     libraryDependencies ++= Seq(
       "org.scala-js" %%% "scalajs-dom" % "0.9.1",
-      "com.lihaoyi" %%% "upickle" % "0.4.4",
-      "io.scalajs.npm" %%% "socket-io-client" % "0.3.0.3",
       "com.fasterxml.jackson.core" % "jackson-annotations" % "2.8.0"
     )
   )
--- jvm/src/main/scala/lars/Main.scala
+++ jvm/src/main/scala/lars/Main.scala
@@ -1,46 +1,17 @@
 package lars
 
-import scala.collection.JavaConversions._
 import lars.core.celestial.body.standard._
 import lars.core.celestial.container.System
 import lars.core.math.Vec2
-import com.corundumstudio.socketio.listener._
-import com.corundumstudio.socketio.{AckRequest, Configuration, SocketIOClient, SocketIOServer}
 import lars.application.LARSApplication
 import lars.core.celestial.CelestialConstants
 import lars.core.physics.units.Time
-import lars.resource.SystemResource
 
 object Main {
   var paused = false
 
   def main(args: Array[String]) {
-
-    val systemName = "Sol"
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Setup
-    //
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
     new LARSApplication().run("server", "jvm/src/main/resources/config.yml")
-
-    val config = new Configuration
-    config.setHostname("localhost")
-    config.setPort(9092)
-    config.getSocketConfig.setReuseAddress(true)
-    val socketio = new SocketIOServer(config)
-    socketio.addEventListener[Array[Byte]]("planets", classOf[Array[Byte]], new DataListener[Array[Byte]] {
-      override def onData(client: SocketIOClient, data: Array[Byte], ackRequest: AckRequest): Unit = {
-        SystemResource.write(systemName) match {
-          case Some(json: String) => client.sendEvent("planets", json)
-          case None =>
-        }
-
-      }
-    })
-    socketio.start()
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //
@@ -75,22 +46,6 @@ object Main {
       }
     }).start()
 
-    new Thread(new Runnable {
-      override def run(): Unit = {
-        while(runLoop) {
-          Thread.sleep(100)
-          socketio.getAllClients.foreach((client: SocketIOClient) => {
-            SystemResource.write(systemName) match {
-              case Some(json: String) => client.sendEvent("planets", json)
-              case None =>
-            }
-
-          })
-        }
-
-      }
-    }).start()
-
     while(runLoop) {
 //      Thread.sleep(1000)
 //      if(!paused) system.observe()
@@ -100,8 +55,6 @@ object Main {
 //        xygon.observe(Time.second)
       }
     }
-
-    socketio.stop()
 
     println("LARS Core stopped.")
   }
