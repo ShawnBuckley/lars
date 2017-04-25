ba0967972cd57162be3a65d463c07b0bd936a926
Shawn Buckley <1851460+ShawnBuckley@users.noreply.github.com>
2017-04-24 22:00:40 -0400

    Switch css to scss

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,15 +1,24 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 
 const baseDir = 'src/main/';
+
+gulp.task('scss', function() {
+  return gulp.src(baseDir + 'scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('sourcemaps/'))
+    .pipe(gulp.dest(baseDir + 'webapp/'))
+});
 
 gulp.task('typescript', function() {
   return gulp.src(baseDir + 'typescript/**/*.ts')
     .pipe(sourcemaps.init())
     .pipe(ts.createProject(baseDir + 'typescript/tsconfig.json')())
-    .pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write('sourcemaps/'))
     .pipe(gulp.dest(baseDir + 'webapp/'));
 });
 
-gulp.task('default', ['typescript']);
+gulp.task('default', ['typescript', 'scss']);
